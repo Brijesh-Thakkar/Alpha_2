a4b16c54a690d88d2c2f6891f2b6b0d09a59b842
Brijesh-Thakkar <brijeshthakkariitian5126@gmail.com>
2025-06-21 23:28:00 +0530

    21 june

--- src/pages/FirstPage.tsx
+++ src/pages/FirstPage.tsx
@@ -1,3 +1,4 @@
+
 import React, { useRef, useEffect, useState } from 'react';
 import './FirstPage.css';
 import './AboutUs.css';
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow access from LAN or external URLs
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'c91b-2405-201-200a-900a-bc7b-9b7c-ec46-2c2e.ngrok-free.app', // Replace with your actual ngrok domain
+    ]
+  }
 })
